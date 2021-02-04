611a8c8a739808d9a53176e12712def8a4679273
ayoajibola <aji21001@byui.edu>
2021-02-04 17:05:43 +0100

    initial commit

--- scripts/poundsToKilogram.js
+++ scripts/poundsToKilogram.js
@@ -1,4 +1,5 @@
-
+/*This JavaScript code accepts input as numbers in pounds
+and convert to Kilogram*/
 	function poundsToKilogram() {
 	//INPUT: x is the number entered by user while y is the conversion factor
 	 let x = parseFloat(document.getElementById("number").value);
