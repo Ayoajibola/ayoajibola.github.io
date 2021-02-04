1eedb9c25319d4ea1501b3d936a130b248d6a370
ayoajibola <aji21001@byui.edu>
2021-02-04 13:40:25 +0100

    initial commit

---
+++ scripts/poundsToKilogram.js
@@ -0,0 +1,10 @@
+
+	function poundsToKilogram() {
+	//INPUT: x is the number entered by user while y is the conversion factor
+	 let x = parseFloat(document.getElementById("number").value);
+	 const y = 0.453592;
+    //PROCESSING: Convert number to kilogram by multiplying with the constant.
+     let z = x*y;
+	 let r = z.toFixed(1);
+	//OUTPUT: Display the result in kilograms and rounded to 1 decimal place.
+	 document.getElementById("output").innerHTML = r + "Kilograms";}
