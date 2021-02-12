9440f8a130ab1cfe0fca1811104c1b1752bf30e9
ayoajibola <aji21001@byui.edu>
2021-02-12 18:08:06 +0100

    initial commit

---
+++ scripts/calculator.js
@@ -0,0 +1,45 @@
+ /*This javaScript programme performes arithmetic operation on two numbers and the arithmetic operator
+  provided by the user. The result   of the calculation is then compared with that of the user and 
+  then a declaration is made  as to whether the user is correct or wrong*/
+
+function calculate() {
+    /*INPUT: User provides the numbers to be operated upon and chooses the operator.
+user also inputs the result*/
+    let x = parseFloat(document.getElementById('number1').value)
+    let y = document.getElementById('operator').value
+    let z = parseFloat(document.getElementById('number2').value)
+    let w = parseFloat(document.getElementById('result').value)
+
+   
+   
+
+    //PROCESSING: Calculation is done based on the four variables provided.
+   
+    let r = 0;  
+
+    switch (y) {
+        case '+':
+            r = x + z
+            break
+        case '-':
+            r = x - z
+            break
+        case '/':
+            r = x / z
+            break
+        case '*':
+            r = x * z
+            
+    }
+ //Compare program result with that of user.
+
+ 
+    if(r === w) {
+        document.getElementById('output').innerHTML = "Correct!";
+       
+    } else {
+      document.getElementById('output').innerHTML = "Incorrect!";
+    }
+    
+    //OUTPUT: Indicate if user is right or otherwise.
+}
