c00063f7bbca6dbbce7f39dce9244dd6c9f171cf
ayoajibola <aji21001@byui.edu>
2021-02-25 23:12:42 +0100

    innitial commit

---
+++ scripts/sumOdds.js
@@ -0,0 +1,19 @@
+function sumOdds() {
+    /*This program asks the user for an integer n and then outputs the sum of all the
+       odd integers between 1 and n, inclusive. */
+
+    //INPUT Program accepts input from user.
+    let n = parseInt(document.getElementById("number").value);
+
+    /*PROCESSING For the entered integer, add all odd numbers between 1 and the integer
+    inclusive */
+    let sum = 0;
+
+    for (let i = 1; i <= n; i += 2) {
+      sum += i;
+    }
+    sum = parseInt((document.getElementById("output").innerHTML = sum));
+
+    //OUTPUT Display the sum of the added numbers.
+    alert(sum);
+  }
